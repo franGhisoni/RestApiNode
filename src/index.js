6beb7c3005f6138be7b0a37539e7ec86a86550b3
franGhisoni <ghisonifran@gmail.com>
2023-05-03 13:07:23 -0300

    para test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const morgan = require('morgan')
+const cors = require('cors');
+const morgan = require('morgan');
+const mercadopago = require('mercadopago');
 
 // Configuración del puerto
 const PORT = 8080;
+
+
+
+
+
+
+//MERCADOPAGO (por el momento voy a hacer todo aca mismo por que asi esta en el ejemplo de mercado apgo)
+
+
+//Configuracion MercadoPago 
+mercadopago.configure({
+	access_token: "<ACCESS_TOKEN>",
+});
+
+
+// app.use(express.static("../../client/html-js"));
+
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+		items: [
+			{
+				title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity),
+			}
+		],
+		back_urls: {
+			"success": "http://localhost:8080/feedback",
+			"failure": "http://localhost:8080/feedback",
+			"pending": "http://localhost:8080/feedback"
+		},
+		auto_return: "approved",
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function (req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
+});
+
+
+
+
+
 
 //middlewares
 app.use(morgan('dev'));
