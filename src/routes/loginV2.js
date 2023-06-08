90255d4a8410a09f1ad4aaca3177b4877c305e58
franGhisoni <ghisonifran@gmail.com>
2023-06-08 19:19:28 -0300

    login fix

--- src/routes/loginV2.js
+++ src/routes/loginV2.js
@@ -1,82 +1,67 @@
 const { Router } = require('express');
-const router= Router();
+const router = Router();
 const sdk = require('api')('@holded/v1.0#3cm531nlbw08qsz');
-const User = require('../models/user')
+const User = require('../models/user');
 
 router.get('/v2/login', async (req, res) => {
+  const { email, password } = req.query;
 
-    const { email, password } = req.query;
+  // Obtener todos los contactos de Holded con el SDK
+  sdk.auth('343654e3d1014f792344a19ee8f40503');
+  let { data } = await sdk.listContacts();
 
-    // Obtener todos los contactos de Holded con el SDK
-    sdk.auth('343654e3d1014f792344a19ee8f40503');
-    let { data } = await sdk.listContacts();
-  
-    // Buscar el contacto que coincide con el correo electrónico y la contraseña proporcionados
-    const user = data.find((contact) => contact.email === email && String(contact.socialNetworks.website) === String(password) );
-    console.log("user.socialNetworks.website")
-    console.log(String(user.socialNetworks.website))
-    console.log("password")
-    console.log( String(password))
-    
-    if (!user) {
-      res.status(401).send({ error: 'Credenciales inválidas' });
-      return;
-    }else {
-      try {
-        let { data } = await sdk.listDocuments({docType: 'invoice'});
-        const listaProductos = []; // Crear una lista vacía de FACTURAS 
-        let productsOwn=[];
-        let servicesOwn = [];
-        let facturas = [];
-        console.log("data[0]")
-        console.log(JSON.parse(data))
-        console.log("fin de data[0]")
+  // Buscar el contacto que coincide con el correo electrónico y la contraseña proporcionados
+  const user = data.find(
+    (contact) => contact.email === email && String(contact.socialNetworks?.website) === String(password)
+  );
 
-        for (const factura of JSON.parse(data)) {
-          listaProductos.push(factura); // Agregar cada producto a la lista
-          
-          if (factura.contact === user.id) {
-            facturas.push(factura.id);
-            factura.products.forEach((product) => {
-              if (product.serviceId) {
-                servicesOwn.push(product);
-              } else {
-                productsOwn.push(product.name);
-              }
-              });
-          }
+  if (!user) {
+    res.status(401).send({ error: 'Credenciales inválidas' });
+    return;
+  } else {
+    try {
+      let { data } = await sdk.listDocuments({ docType: 'invoice' });
+      const listaProductos = []; // Crear una lista vacía de FACTURAS
+      let productsOwn = [];
+      let servicesOwn = [];
+      let facturas = [];
+
+      for (const factura of JSON.parse(data)) {
+        listaProductos.push(factura); // Agregar cada producto a la lista
+
+        if (factura.contact === user.id) {
+          facturas.push(factura.id);
+          factura.products.forEach((product) => {
+            if (product.serviceId) {
+              servicesOwn.push(product);
+            } else {
+              productsOwn.push(product.name);
+            }
+          });
         }
-        
-        console.log("productsOwn");
-        console.log(productsOwn);
-        // console.log("listaProductos");
-        // console.log(listaProductos);
-
-        let userDTO = new Object();
-        userDTO["name"]= user.name;
-        userDTO["email"]= user.email;
-        userDTO["mobile"]= user.mobile;
-        userDTO["id"]= user.id;
-        userDTO["productos"]= productsOwn;
-        userDTO['password']=String(user.socialNetworks.website)
-        userDTO['servicios']=servicesOwn;
-        userDTO['facturas']=facturas;
-        userDTO['fechaNac']=user.iban;
-        userDTO['genero']=user.swift;
-        userDTO['lang']=user.defaults.language;
-        userDTO['address']= user.billAddress;
-
-        console.log("user DTO:")
-        console.log(userDTO);
-
-        res.status(201).send(userDTO); // Devolver la lista de productos
-        module.exports = { router, userDTO };
-
-      } catch (err) {
-        console.error(err);
-        res.status(500).send({ error: err.message }); 
       }
+
+      let userDTO = {
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile,
+        id: user.id,
+        productos: productsOwn,
+        password: String(user.socialNetworks?.website),
+        servicios: servicesOwn,
+        facturas: facturas,
+        fechaNac: user.iban,
+        genero: user.swift,
+        lang: user.defaults.language,
+        address: user.billAddress
+      };
+
+      res.status(201).send(userDTO); // Devolver la lista de productos
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
     }
+  }
 });
 
 module.exports = router;
